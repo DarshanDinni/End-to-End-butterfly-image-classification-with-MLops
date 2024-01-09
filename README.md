7381edec5b32f63644c916df840e15d62515b4d3
DarshanDinni <darshandinni1@gmail.com>
2024-01-09 22:39:03 +0000

    Requirement installed and readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# End-to-End-butterfly-image-classification-with-MLops
+# Butterfly Image Classifier
+
+## Overview
+
+The Butterfly Image Classification project revolves around a diverse dataset featuring 75 distinct classes of butterflies. Comprising over 1000 labeled images, including validation samples, each image is exclusively associated with a single butterfly category.
+
+The training dataset, encapsulated in Training_set.csv, contains labeled images crucial for model training. Additionally, the Testing_set.csv includes a list of images in the test folder, where the objective is to predict the corresponding labels.
+
+This project aims to develop a robust machine learning model capable of accurately classifying butterfly species based on images.
+
+## Badges
+
+[![Build Status](...)](...)
+[![License: MIT](...)](...)
+
+## Table of Contents
+- [Title and Overview](#butterfly-image-classifier)
+- [Badges](#badges)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [License](#license)
+- [Acknowledgements](#acknowledgements)
+
+## Installation
+1. **Clone the repository:**
+
+    ```
+    https://github.com/DarshanDinni/End-to-End-butterfly-image-classification-with-MLops.git
+    ```
+
+2. **Install dependencies:** 
+
+    ```
+    pip3 install -r requirements.txt --index-url https://download.pytorch.org/whl/cu118
+    ```
+
+## Usage
+
+## Contribution
+
+## License
+This project is licensed under the MIT License.
+
+## Acknowledgements
+- **PyTorch:** [Pytorch docs](https://pytorch.org/docs/stable/index.html)
+- **Data source:** [Kaggle](https://www.kaggle.com/) 
+- **Dataset link:** [Click here](https://www.kaggle.com/datasets/phucthaiv02/butterfly-image-classification/data)
--- project_structure_template.py
+++ project_structure_template.py
@@ -1,43 +1,45 @@
+import logging
 import os
 from pathlib import Path
-import logging
 
 # Setting loging configuration
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S"
+)
 
 project_name = "butterflyClassifier"
 
 list_of_files = [
-    '.github/workflows/.gitkeep',
-    f'src/{project_name}/__init__.py',
-    f'src/{project_name}/exception_handling.py',
-    f'src/{project_name}/logger.py',
-    f'src/{project_name}/components/__init__.py',
-    f'src/{project_name}/utils/__init__.py',
-    f'src/{project_name}/config/__init__.py',
-    f'src/{project_name}/config/configuration.py',
-    f'src/{project_name}/pipeline/__init__.py',
-    f'src/{project_name}/entity/__init__.py',
-    f'src/{project_name}/constants/__init__.py',
-    'config/config.yaml',
-    'dvc.yaml',
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/exception_handling.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
     "params.yaml",
-    'requirements.txt',
-    'setup.py',
-    'notebook/trails.ipynb',
-    'template/index.html'
+    "requirements.txt",
+    "setup.py",
+    "notebook/trails.ipynb",
+    "template/index.html",
 ]
 
 for file_path in list_of_files:
     file_path = Path(file_path)
     file_dir, file_name = os.path.split(file_path)
-    
-    if file_dir != '':
+
+    if file_dir != "":
         os.makedirs(file_dir, exist_ok=True)
         logging.info(f"Creating folder: {file_dir}")
-    
+
     if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
-        with open(file_path, 'w') as f:
+        with open(file_path, "w") as f:
             pass
         logging.info(f"Creating file: {file_path}")
     else:
